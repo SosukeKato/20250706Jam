06a64fd1184e5ca934d239f33341d65160177a59
ryotakonishi <ryota.konishi@icloud.com>
2025-07-06 15:29:36 +0900

    [add]二段ジャンプ、移動処理追加

--- Assets/_WorkSpace/Konisi/player.cs
+++ Assets/_WorkSpace/Konisi/player.cs
@@ -1,12 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class player : MonoBehaviour
 {
+    [SerializeField] int MoveSpeed;
     [SerializeField] float JumpForce = 350;
 
     private Rigidbody2D _rig = null;
+    private bool _isGrounded = false;
+    private bool _isDoubleJump = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,13 +17,44 @@ public class player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        Jump();
+        if (Input.GetKey(KeyCode.A))
         {
-            Jump();
+            transform.position += new Vector3(-MoveSpeed * Time.deltaTime, 0); 
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(MoveSpeed * Time.deltaTime, 0); 
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            _isDoubleJump = true;
+            _isGrounded = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            _isGrounded = false;
         }
     }
     private void Jump()
     {
-        _rig.AddForce(new Vector2(0, JumpForce));
+        if (Input.GetKeyDown(KeyCode.W) && _isGrounded)
+        {
+            _rig.AddForce(new Vector2(0, JumpForce));
+            Debug.Log("ジャンプ一段目");
+        }
+        if (Input.GetKeyDown(KeyCode.W) && !_isGrounded && _isDoubleJump)
+        {
+            _isDoubleJump = false;
+            _rig.velocity = new Vector2(_rig.velocity.x, 0);
+            _rig.AddForce(new Vector2(0, JumpForce));
+            Debug.Log("ジャンプ二段目");
+        }
     }
 }
