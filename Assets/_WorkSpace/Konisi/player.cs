d1a130ce5929c64c9b02103142999df3f5733aa1
ryotakonishi <ryota.konishi@icloud.com>
2025-07-06 14:10:13 +0900

    [add]rigibodyコンポーネント取得

--- Assets/_WorkSpace/Konisi/player.cs
+++ Assets/_WorkSpace/Konisi/player.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
-    Rigidbody _rig = null;
+    Rigidbody2D _rig = null;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rig = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
