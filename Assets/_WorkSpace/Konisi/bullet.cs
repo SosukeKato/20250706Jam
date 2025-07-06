702414112c629b6cb35022e8580184e9f81aa1b6
ryotakonishi <ryota.konishi@icloud.com>
2025-07-06 15:59:23 +0900

    [add]弾移動用

--- Assets/_WorkSpace/Konisi/bullet.cs
+++ Assets/_WorkSpace/Konisi/bullet.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class bullet : MonoBehaviour
 {
+    [SerializeField] int BulletSpeed;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,6 @@ public class bullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+         transform.Translate(BulletSpeed * Time.deltaTime,0,0);
     }
 }
