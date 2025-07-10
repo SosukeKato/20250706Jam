1238cc338873e1a017ee77ebc11fda6f6b899e05
ryotakonishi <ryota.konishi@icloud.com>
2025-07-10 20:30:02 +0900

    [add]始まった時にUIを表示するUI追加

--- Assets/_WorkSpace/Konisi/PlayerHP.cs
+++ Assets/_WorkSpace/Konisi/PlayerHP.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHP : MonoBehaviour
 {
+    [SerializeField] Image PlayerHPUI;
+
+    private player _player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _player = FindAnyObjectByType<player>();
+        for (int i = 0; i < _player.PlayerHP; i++)
+        {
+            Instantiate(PlayerHPUI);
+        }
     }
 
     // Update is called once per frame
