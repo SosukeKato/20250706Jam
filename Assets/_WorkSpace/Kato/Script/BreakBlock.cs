0537a7b94a0998dd249cb333667750e84115de40
SosukeKato <dokindokinpiano@gmail.com>
2025-07-13 11:22:59 +0900

    [Update]BreakBlock.csにPlayerに触れたら2秒待って消える処理を追加

--- Assets/_WorkSpace/Kato/Script/BreakBlock.cs
+++ Assets/_WorkSpace/Kato/Script/BreakBlock.cs
@@ -1,18 +1,19 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BreakBlock : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine("BreakWaitTime");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator BreakWaitTime()
     {
-        
+        yield return new WaitForSeconds(2);
+        Destroy(gameObject);
     }
 }
