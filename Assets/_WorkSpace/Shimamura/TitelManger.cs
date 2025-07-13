fe30a150c864e82b9a5d4509360c18878d81c17c
shimamura <shimakou779@gmail.com>
2025-07-13 10:44:48 +0900

    [add]タイトルシーン作成

---
+++ Assets/_WorkSpace/Shimamura/TitelManger.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitelManger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void StartGame()//タイトルシーンへ移動
+    {
+        // "GameScene" は遷移先のシーン名
+        SceneManager.LoadScene("StageScene");
+    }
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        // ビルド後にアプリケーションを終了
+        Application.Quit();
+#endif
+    }
+}
